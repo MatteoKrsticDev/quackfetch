eebcabf2b44c13d917fd93bdabdd239d34d82978
MatteoKrsticDev <krsticmatteo@gmail.com>
2022-05-22 14:25:15 +0200

    installer file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 python3 -m pip install -r requirements.txt
+cd duckfetch
 sudo cp duckfetch.py /bin/duckfetch
 cd ..
 sudo alias duckfetch='python3 /usr/local/bin/duckfetch'
 if [ -n "$ZSH_VERSION" ]; then
     echo "zsh detected"
-    echo "alias duckfetch='python3 /usr/local/bin/duckfetch'" >> ~/.zshrc
+    echo "alias duckfetch='python3 /bin/duckfetch'" >> ~/.zshrc
     source ~/.zshrc
 else
     echo "bash detected"
-    echo "alias duckfetch='python3 /usr/local/bin/duckfetch'" >> ~/.bashrc
+    echo "alias duckfetch='python3 /bin/duckfetch'" >> ~/.bashrc
     source ~/.bashrc
 fi
